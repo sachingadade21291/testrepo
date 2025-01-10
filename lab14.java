86c3acd4ddbb8aadaea02863bf287091fd4d2bfa
sachin <sachingadade21291@gmail.com>
2025-01-10 22:56:04 +0530

    second commit

---
+++ lab14.java
@@ -0,0 +1,11 @@
+class lab14{
+public static void main(String[] args){
+double g=29.28;
+double h=26.29;
+double j=17.6;
+double k=g*h*j;
+double l=g/h/j;
+System.out.println(k);
+System.out.println(l);
+}
+}

6819126da205e4ac7f602e3e5b3a470abf2bf916
Sachin Gadade <sachingadade21291@gmail.com>
2025-01-11 22:20:45 +0530

    Add files via upload

---
+++ Lab15.java
@@ -0,0 +1,12 @@
+class Lab15{
+public static void main(String[] args){
+
+String name="Sachin";
+int age=27;
+
+System.out.println("My name is"+name);
+System.out.println("My age is"+age);
+
+}
+
+}
